be59b27c7b37b5b82348d87927ff79aadaa50b13
Lucio Dery <ldery@stanford.edu>
2017-05-15 05:58:44 -0700

    working model pipeline ! clearclear

--- code/main.py
+++ code/main.py
@@ -1,10 +1,11 @@
-import optparse import OptionParser
+from optparse import OptionParser
 from datagenerator import getData
 import numpy as np
 from ptrNet import PointerNetwork
+import tensorflow as tf
 # TODO: Trainable initial state might be needed
 # TODO: Implement glimpses
-# TODO: 
+# TODO: Fix bidirectional aspect! 
 def getOptimizer(options):
 	lr = options.lr  # might want to do more complicated things 
 	if options.optim == "Adam":
@@ -17,13 +18,21 @@ def getOptimizer(options):
 		return tf.train.GradientDescentOptimizer(learning_rate=lr)
 
 def get_corrected_inputs(inputs, targets, seq_lens):
-	indices = tf.argmax(targets, 1)
-	first_ind = tf.range()
-	indices = tf.reshape(indices, tf.shape(inputs)[:2])
-	retun tf.gather_nd(inputs, indices)
+	indices = tf.cast(tf.argmax(targets, 1),  tf.int32)
+	# modified from https://github.com/devsisters/pointer-network-tensorflow/blob/master/layers.py
+	replicated_first_indices = tf.range(tf.shape(indices)[0])
+	rank = len(indices.get_shape())
+	if rank == 2:
+	    replicated_first_indices = tf.tile(
+	        tf.expand_dims(replicated_first_indices, dim=1),
+	        [1, tf.shape(indices)[1]])
+	# print replicated_first_indices, indices, targets
+	# print tf.shape(replicated_first_indices), tf.shape(indices)
+	nd_indices = tf.stack([replicated_first_indices, indices], axis=rank)
+	return tf.stop_gradient(tf.gather_nd(inputs, nd_indices))
 
 def model_string(options):
-	model_str = 
+	model_str = 'will_do_this_later'
 	return model_str
 
 def buildModel(options):
@@ -34,40 +43,43 @@ def buildModel(options):
 
 	with tf.variable_scope("decoder"):
 		# need to transform inputs
-		train_inputs = get_corrected_inputs(attention_states, targets, seq_lens) # reshuffles the inputs to be the correct shape
-		outputs, final_state, final_context_state = ptr_net.decode(train_inputs, targets, attention_states, encoder_end_states, seq_lens, train=1)
+		train_inputs = get_corrected_inputs(inputs, targets, seq_lens) # reshuffles the inputs to be the correct shape
+		outputs, final_state  = ptr_net.decode(train_inputs, attention_states, encoder_end_states, seq_lens, train=1)
 
 	with tf.variable_scope("decoder", reuse=True):
-		predictions, final_state, final_context_state = ptr_net.decode(inputs, attention_states, encoder_end_states, seq_lens, train=0)
+		predictions, final_state = ptr_net.decode(inputs, attention_states, encoder_end_states, seq_lens, train=0)
 
 	outputs = tf.reshape(outputs, [-1, options.max_len])
-	loss = tf.reduce_mean(tf.softmax_crossentropy_with_logits(targets, outputs))
-	optimizer = getOptimizer(optimizer)
+	targets_flat = tf.reshape(targets, [-1, options.max_len])
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=outputs, labels=targets_flat))
+	optimizer = getOptimizer(options)
 	train_op = optimizer.minimize(loss)
-	return inputs, targets, predictions, loss, train_op
+	return inputs, targets, seq_lens, predictions, loss, train_op
 
 
 
 def train(data, options):
+	tf.reset_default_graph()
 	model_str = model_string(options)
 	x_train, y_train, train_seq_lens = data['train'] 
 	x_val , y_val, val_seq_lens  = data["val"]
 
-	init = tf.global_varialbles_initializer()
-	input_x, input_y, predictions, loss, train_op = buildModel()
+	input_x, input_y, seq_lens,  predictions, loss, train_op = buildModel(options)
 
-	merged = tf.merge_all_summaries()
-	saver = tf.train.saver()
-	with tf.Session() as sess:
-		train_writer = tf.train.SummaryWriter(options.log_dir + "/" + model_str + "/train", sess.graph())
-		test_writer = tf.train.SummaryWriter(options.log_dir + "/" + model_str + "/test", sess.graph())
+	# merged = tf.summary.merge_all()
+	saver = tf.train.Saver()
+	sess = tf.Session()
+	with sess.as_default():
+		# train_writer = tf.summary.FileWriter(options.log_dir + "/" + model_str + "/train", sess.graph())
+		# test_writer = tf.summary.FileWriter(options.log_dir + "/" + model_str + "/test", sess.graph())
+		init = tf.global_variables_initializer()
 		sess.run(init)
 		if options.load_ckpt:
 			print "Loading from checkpoint..."
-            saver.restore(sess, options.ckpt_dir + "/" + model_str + "/model.ckpt")
+			saver.restore(sess, options.ckpt_dir + "/" + model_str + "/model.ckpt")
         print "Training Network"
-		epoch_losses = []
-		for epoch in xrange(options.nb_epoch):
+        epoch_losses = []
+        for epoch in xrange(options.nb_epoch):
 			epoch_loss = 0.0
 			num_batches = len(x_train)
 			permutation = np.random.permutation(num_batches)
@@ -77,20 +89,21 @@ def train(data, options):
 			for i in xrange(num_batches):
 				curr_iter = (epoch * num_batches + i)
 
-				feed_dict = {input_x : x_train[i], input_y : y_train[i]}
-				summary, loss, _ = sess.run([merged, loss, train_op], feed_dict = feed_dict)
-				train_writer.add_summary(summary, curr_iter)
+				feed_dict = {input_x : x_train, input_y : y_train, seq_lens : train_seq_lens}
+				loss_val, _ = sess.run([loss, train_op], feed_dict = feed_dict)
+				# train_writer.add_summary(summary, curr_iter)
 				if curr_iter % options.iter_per_chkpt == 0:
 					saver.save(sess, options.ckpt_dir + "/" + model_str + "/model.ckpt")
 
-				feed_dict = {input_x : x_val[i], input_y : y_val[i]}
-				summary, val_loss = sess.run([merged, loss], feed_dict = feed_dict)
-				test_writer.add_summary(summary, curr_iter)
+				feed_dict = {input_x : x_val, input_y : y_val, seq_lens : val_seq_lens}
+				val_loss = sess.run([loss], feed_dict = feed_dict)[0]
+				# test_writer.add_summary(summary, curr_iter)
 
-				print("Epoch :  ", epoch, " Batch : " , i,  ", Minibatch Train Loss= " + "{:.6f}".format(loss))
+				print("Epoch :  ", epoch, " Batch : " , i,  ", Minibatch Train Loss= " + "{:.6f}".format(loss_val))
+
 				print("Epoch :  ", epoch, " Batch : " , i,  ", Minibatch Val Loss= " + "{:.6f}".format(val_loss))
 				print("-----------------------------")
-				epoch_loss += loss
+				epoch_loss += loss_val
 			epoch_losses.append(epoch_loss/num_batches)
 	return epoch_losses
 
@@ -100,20 +113,20 @@ def train(data, options):
 
 if __name__ == "__main__":
 	parser = OptionParser()
-	parser.add_option("--batch_size", type=int, dest='batch_size', default=64)
-	parser.add_option("--epochs", type=int, dest="nb_epoch", default=20)
-	parser.add_option("--lr", type=float, dest="lr", default=1e-3)
-	parser.add_option("--optim", type=string, dest="optim", default="Adam")
-	parser.add_option("--bidirect", actions="store_true", dest="bidirectional", default=True)
-	parser.add_option("--puzzle_width", type=int, dest="puzzle_width", default=5)
-	parser.add_option("--puzzle_height", type=int, dest="puzzle_height", default=5)
-	parser.add_option("--log_dir", type=string, dest="log_dir", default="logs")
-	parser.add_option("--ckpt_dir", type=string, dest="ckpt_dir", default="checkpoints")
-	parser.add_option("--load_ckpt", type=string, dest="load_ckpt", default="")
+	parser.add_option("--batch_size", type=int, dest='batch_size', default=5)
+	parser.add_option("--epochs", type=int, dest="nb_epoch", default=200)
+	parser.add_option("--lr", type=float, dest="lr", default=1e-1)
+	parser.add_option("--optim", type=str, dest="optim", default="Adam")
+	parser.add_option("--bidirect", action="store_true", dest="bidirectional", default=False)
+	parser.add_option("--puzzle_width", type=int, dest="puzzle_width", default=2)
+	parser.add_option("--puzzle_height", type=int, dest="puzzle_height", default=2)
+	parser.add_option("--log_dir", type=str, dest="log_dir", default="logs")
+	parser.add_option("--ckpt_dir", type=str, dest="ckpt_dir", default="checkpoints")
+	parser.add_option("--load_ckpt", type=str, dest="load_ckpt", default="")
 	parser.add_option("--iter_per_chkpt", type=int, dest="iter_per_chkpt", default="10")
-	parser.add_option("--max_len", type=int, dest="max_len", default=25)
-	parser.add_option("--ptrnet_input_dim", type=int, dest="input_dim", default=100)
-	parser.add_option("--ptrnet_hidden_dim", type=int, dest="hidden_dim", default=100)
+	parser.add_option("--max_len", type=int, dest="max_len", default=4)
+	parser.add_option("--ptrnet_input_dim", type=int, dest="input_dim", default=1)
+	parser.add_option("--ptrnet_hidden_dim", type=int, dest="hidden_dim", default=3)
 
 	options, args = parser.parse_args()
 
--- code/ptrNet.py
+++ code/ptrNet.py
@@ -1,6 +1,7 @@
 import tensorflow as tf 
-from tf.contrib.layers import variance_scaling_initializer
+# from tensorflow.contrib.layers import variance_scaling_initializer
 from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops import array_ops
 
 class PointerNetwork(object):
 
@@ -9,76 +10,90 @@ class PointerNetwork(object):
 		self.input_dim = input_dim
 		self.bidirectional = bidirectional
 		self.hidden_dim = hidden_dim
-		self.init = variance_scaling_initializer()
+		self.init =  tf.contrib.layers.variance_scaling_initializer()
+		self.decoder_cell = tf.contrib.rnn.LSTMCell(self.hidden_dim, initializer = self.init)
 
 
 		# self.inputs, self.seq_lengths = self.get_placeholders(max_len, input_dim) will need this for initial testing
 
 		
 	def get_input_placeholders(self):
-		inputs = tf.placeholder(tf.float32, [None, self.max_len, self.input_dim], name='Encoder_input')
+		inputs = tf.placeholder(tf.float32, shape=(None, self.max_len, self.input_dim), name='Encoder_input')
 		seq_lengths = tf.placeholder(tf.int32, [None, ], name='seq_input')
-		targets = tf.placeholder(tf.int32, [None, self.max_len], name="targets")
+		targets = tf.placeholder(tf.int32, [None, self.max_len, self.max_len], name="targets")
 		return inputs, seq_lengths, targets
 
 	def encode(self, inputs, seq_lengths):
 		if not self.bidirectional:
-			cell = tf.nn.rnn_cell.LSTMCell(hidden_dim, initializer = self.init)
-			output, output_state = tf.nn.dynamic_rnn(cell, inputs)
+			cell = tf.contrib.rnn.LSTMCell(self.hidden_dim, initializer = self.init)
+			output, output_state = tf.nn.dynamic_rnn(cell, inputs, sequence_length=seq_lengths, dtype=tf.float32)
 		else:
-			cell_fw = tf.nn.rnn_cell.LSTMCell(hidden_dim, initializer = self.init)
-			cell_bw = tf.nn.rnn_cell.LSTMCell(hidden_dim, initializer = self.init)
-			output_fw_bw, output_states = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, inputs , sequence_length=seq_lengths)
-			output = output_fw_bw[0] + output_fw_bw[1] # assuming we are just summing the forward and backwared activations
-			output_state = output_states[0] + output_states[1]
+			# TODO: Not working ! Need to fix
+			cell_fw =  tf.contrib.rnn_cell.LSTMCell(self.hidden_dim, initializer = self.init)
+			cell_bw =  tf.contrib.rnn_cell.LSTMCell(self.hidden_dim, initializer = self.init)
+			output_fw_bw, output_states = tf.contrib.bidirectional_dynamic_rnn(cell_fw, cell_bw, inputs , sequence_length=seq_lengths, dtype=tf.float32)
+			output = tf.add(output_fw_bw[0], output_fw_bw[1]) # assuming we are just summing the forward and backwared activations
+			output_state = tf.add(output_states[0], output_states[1])
 
 		return output, output_state
 
 # might need to stop gradients for inputs when doing tests
 # should probably implement glimpses : https://github.com/devsisters/pointer-network-tensorflow/blob/master/layers.py
-	def decode(self, inputs, targets, enc_out, enc_end_state, seq_lengths, train=1):
+	def decode(self, inputs, enc_out, enc_end_state, seq_lengths, train=1):
 		# note that here seq_lengths is a numpy array 
 		with vs.variable_scope("decoder_scope") as scope:
 			maximum_length = tf.convert_to_tensor(self.max_len, tf.int32)
 			def attention(hidden, scope="Attention"):
 				with vs.variable_scope(scope):
 					if hidden is None:
-						return tf.zeros([tf.shape(inputs)[0], self.max_len], tf.float32), tf.zeros([tf.shape(inputs)[0], self.hidden_dim], tf.float32)
+						return tf.zeros([tf.shape(enc_out)[0], self.hidden_dim], tf.float32), tf.zeros([tf.shape(enc_out)[0], tf.shape(inputs)[2]], tf.float32)
 
-					att_W_in = vs.get_variable("att_W_in", (1, self.hidden_dim, self.hidden_dim), self.init)
-					att_W_h  =  vs.get_variable("att_W_h", (self.hidden_dim, self.hidden_dim),  self.init)
-					att_V = vs.get_variable("att_V", (self.hidden_dim, ), self.init)
+					att_W_in = vs.get_variable("att_W_in", (1, self.hidden_dim, self.hidden_dim), initializer=self.init)
+					att_W_h  =  vs.get_variable("att_W_h", (self.hidden_dim, self.hidden_dim),  initializer=self.init)
+					att_V = vs.get_variable("att_V", (self.hidden_dim, ), initializer=self.init)
 
 					enc_ref = tf.nn.conv1d(enc_out, att_W_in, 1, "VALID", name="intermediate")
+					_, hidden = tf.unstack(hidden, axis=0)
+					# print c, hidden, att_W_h, tf.matmul(hidden, att_W_h)
 					enc_query = tf.expand_dims(tf.matmul(hidden, att_W_h), 1, name="exp_enc_query")
 					enc_query = tf.tile(enc_query, [1, self.max_len, 1], name = "tiled_enc_query")
-					scores = tf.reduce_sum(att_V*tf.tanh(enc_ref + enc_query), [-1]) # doing the sum of the last dimension as dot product
-					inds = tf.argmax(scores, 1)
-					next_inputs = tf.stop_gradient(tf.gather(inputs, inds))
+					scores = tf.reduce_sum(att_V*tf.tanh(enc_ref + enc_query), [-1], name='scores') # doing the sum of the last dimension as dot product
+					
+					inds = tf.cast(tf.argmax(scores, 1),  tf.int32)
+					replicated_first_indices = tf.range(tf.shape(inds)[0])
+					inds = tf.stack([inds, replicated_first_indices], axis=1)
+
+					next_inputs = tf.stop_gradient(tf.gather_nd(inputs, inds))
 					return scores, next_inputs
 
-			decoder_cell = tf.nn.rnn_cell.LSTMCell(hidden_dim, initializer = self.init)
-			if train:
-				decoder_fn = tf.contrib.seq2seq.simple_decoder_fn_train(enc_end_state, scope=scope)
-			else:
-				def decoder_fn(time, cell_state, cell_input, cell_output, context_state):
-					cell_output, next_input = attention(cell_output)
-					if cell_state is None:
-						cell_state = enc_end_state
-						next_input = cell_input
-						done = tf.zeros([tf.shape(inputs)[0], ], dtype=tf.bool)
-					done = tf.cond(tf.greater(time, maximum_length), lambda: tf.ones([tf.shape(inputs)[0],], dtype=tf.bool), lambda: done)
-					return done, cell_state, next_input, cell_output, context_state
+			batch_size = tf.shape(inputs)[0]  # Needed for reshaping.
+	        input_size = tf.shape(inputs)[2]
+	        attn_length = tf.shape(enc_out)[1]
+	        attn_size = tf.shape(enc_out)[2]
+	        outputs = []
+	        # batch_attn_size = array_ops.pack([batch_size, attn_size])
+	        # attns = array_ops.zeros(batch_attn_size, dtype=tf.float32)
 
-			outputs, final_state, final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell, \
-				decoder_fn, inputs, sequence_length = seq_lengths, scope=scope)
+	        # attns.set_shape([None, attn_size])
+	        states = [enc_end_state]
+	        inp = tf.zeros([tf.shape(enc_out)[0], self.input_dim], tf.float32)
+	        inputs = tf.transpose(inputs, [1, 0, 2])
+	        for i in xrange(self.max_len):
+	            if i > 0:
+	                vs.get_variable_scope().reuse_variables()
 
-		return outputs, final_state, final_context_state
-
-
-
-
-		
+	            if train:
+	           		inp = tf.gather(inputs, i)
+	                
+	            # Merge input and previous attentions into one vector of the right size.
+	            cell_output, new_state = self.decoder_cell(inp, states[-1])
+	            # print inp, cell_output, new_state , '\n'
+	            states.append(new_state)
+	            # Run the attention mechanism.
+	            output, inp = attention(new_state)
+	            outputs.append(output)
+		outputs = tf.transpose(tf.stack(outputs), [1, 0, 2])
+		return outputs, states
 
 
 
